ababf853f48985730e93d651d68cc8e008ad2032
kaleemnet <kaleemnet@yahoo.com>
2015-06-09 17:42:34 +0300

    Update

---
+++ js/data-jo.js
@@ -0,0 +1,24 @@
+ /* All admin name must be in CAP */
+
+var rcode2reg = {
+    '1':'NORTH REGION',
+    '2':'CENTRAL REGION',
+    '3':'SOUTH REGION',
+    '4':'No Breakdown per Governorate',
+    '5':'ALL CAMPS IN JORDAN'}
+
+var gcode2gov = {
+    '1704':'KARAK',
+    '1710':'MADABA',
+    '1705':'AMMAN',
+    '1703':'AL AQABAH',
+    '1706':'TAFILAH',
+    '1712':'ZARQA',
+    '1709':'MAAN',
+    '1701':'AJLOUN',
+    '1702':'BALQA',
+    '1707':'IRBID',
+    '1711':'MAFRAQ',
+    '1708':'JARASH',
+    '1':'No Breakdown per Governorate',
+    '2':'ALL CAMPS IN JORDAN'}
